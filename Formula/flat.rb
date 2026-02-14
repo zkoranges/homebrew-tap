48fb9d6182bda1c5904d66f2f7579586f71d09a2
zkoranges <116907692+zkoranges@users.noreply.github.com>
2026-02-14 22:08:18 +0000

    flat 0.3.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/flat.rb
+++ Formula/flat.rb
@@ -1,8 +1,8 @@
 class Flat < Formula
   desc "Flatten codebases into AI-friendly format"
   homepage "https://github.com/zkoranges/flat"
-  url "https://github.com/zkoranges/flat/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "d2691b09131a22dc1f4f6060d499007581eba55ef389ff0624339051e015c1ec"
+  url "https://github.com/zkoranges/flat/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "fd1b9ba0ab54eced4540ef83104a0ab38fb5dbe3816bbf8f375440b5f798127f"
   license "MIT"
 
   livecheck do
